373d94095a8f60c29324ca90ca5978dc5314d907
Little_100 <2662308929@qq.com>
2026-02-13 15:49:17 +0800

    4.0

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,10 @@
+// 在 Windows 上，正常双击启动时隐藏控制台窗口
+// 但如果从命令行启动，仍然可以看到输出
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
 fn main() {
     mc_plugin_market_lib::run()
 }
